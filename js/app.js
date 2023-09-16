7158f1860248ee51f578df8437594474feeb6b40
Ariane Santos <ariane225040@gmail.com>
2023-09-16 17:43:43 -0300

    deploy de arquivos css e js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle')
 const links = document.querySelector('.links')
 
-navToggle.addEventListener('click', function () {
-    // console.log(links.classList.contains("links"));
-    // if(links.classList.contains('show-links')) {
-    //     links.classList.remove('show-links')
-    // } else {
-    //     links.classList.add('show-links')
-    // }      ou
-    
+navToggle.addEventListener('click', function () { 
     links.classList.toggle
     ('show-links')
 })
