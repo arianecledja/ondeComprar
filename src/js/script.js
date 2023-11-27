e150c86865d9feb585c521ce6db274650771152d
Ariane Santos <ariane225040@gmail.com>
2023-11-27 08:02:05 -0300

    script.js updated to fix bugs

--- src/js/script.js
+++ src/js/script.js
@@ -4,29 +4,31 @@ const container = document.getElementById('container')
 const navToggle = document.querySelector('.nav-toggle')
 const links = document.querySelector('.links')
 
-navToggle.addEventListener('click', function () { 
-    links.classList.toggle
-    ('show-links')
-})
-
 function createCard() {
     products.forEach((product) => {
         const box = document.createElement('div')
         box.classList.add('box')
-    
-        box.innerHTML += `
-            <h3 id="${product.id}">${product.id}</h3>
-            <h4>${product.name}</h4 
-            <a href="${product.link}"
-                target="_blank">
-                <img src="./src/assets/images/${product.photo}" alt="${product.description}">
-            </a>  
-        ` 
+        const h3 = document.createElement('h3')
+        const h4 = document.createElement('h4')
+        const imgBox = document.createElement('div')
+        const a = document.createElement('a')
+        const img = document.createElement('img')
+
+        h3.textContent = `${product.id}`
+        h3.id = product.id
+        h4.textContent = `${product.name}`
+        imgBox.classList.add('img-box')
+        a.href = product.link
+        a.target = '_blank'
+        img.src = `./src/assets/images/${product.photo}`
+        a.appendChild(img)
+        imgBox.appendChild(a)
+        box.appendChild(h3)
+        box.appendChild(h4)
+        box.appendChild(imgBox) 
 
         container.appendChild(box)
-        console.log(box);
-    })
-    
+    })  
 }
 
 function init() {
@@ -35,3 +37,13 @@ function init() {
 
 init()
 
+navToggle.addEventListener('click', function () { 
+    links.classList.toggle
+    ('show-links')
+})
+
+document.querySelectorAll('.links li').forEach((li) => {
+    li.addEventListener('click', () => {
+        links.classList.remove('show-links')
+    })
+})
